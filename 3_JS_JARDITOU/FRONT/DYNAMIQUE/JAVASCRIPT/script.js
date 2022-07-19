bb5ee4b7c5b7a17e6edcc501d41299fc1904f0ba
HOCMERT pascal <pascal80000@free.fr>
2022-07-19 08:38:23 +0200

    18 07 2022 soir

---
+++ 3_JS_JARDITOU/FRONT/DYNAMIQUE/JAVASCRIPT/script.js
@@ -0,0 +1,57 @@
+/*
+
+var bouton1= document.getElementById("Id_btn1");
+bouton1.addEventListener("click",clickbtn1);
+
+function clickbtn1(){
+    alert("Vous avez cliqué sur :\n Alert()");
+}
+
+var bouton2= document.getElementById("Id_btn2");
+bouton2.addEventListener("click",clickbtn2);
+
+function clickbtn2(){
+
+    var prenom = prompt("Veuillez saisir votre prénom");
+    if(prenom==null){
+    alert("vous avez cliqué sur Annuler");
+        return;
+    }
+    var age=par(prompt("Veuillez saisir votre âge"));
+    if(isNaN(age)){
+        alert("ERREUR !  recommencez.");
+        return;
+    }
+
+    alert("Voilà:+prenom+\n\nVotre age :"+age);
+}
+
+var bouton3=document.getElementById("Id_btn3");
+bouton3.addEventListener("click",clickbtn3);
+
+function clickbtn3(){
+
+    var reponse = confirm("Veuillez cliquer sur Ok ou bien Annuler")
+    if (reponse == true) {
+        alert("Vous aver cliqué sur Ok")
+    }
+    else {
+        alert("Vous avez cliqué sur Annuler");
+    }
+}
+
+var bouton4= document.getElementById("Id_btn4");
+bouton4.addEventListener("click",clickbtn4);
+
+function clickbtn4() {
+    var prenom = prompt("Veuillez saisir votre prénom");
+    if(prenom==null){
+        alert("Vous avez annulé");
+        }
+        else{
+            console.log("Voici le prénom saisi: "+prenom);
+            alert("Verifiez en console (F12), ce que vous avez saisi...");
+
+        }
+}
+*/

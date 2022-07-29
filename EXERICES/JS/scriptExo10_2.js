8d71676128d7b8801b3f4e000c5930f4879d1ba0
HOCMERT pascal <pascal80000@free.fr>
2022-07-29 11:30:09 +0200

    Ven 29 07 2022

--- EXERICES/JS/scriptExo10_2.js
+++ EXERICES/JS/scriptExo10_2.js
@@ -1,89 +1,140 @@
-// ATTENTION aux variables LOCALES ou GLOBALES
+// ATTENTION aux variables LOtCALES ou GLOBALES
 // si variable locale, càd définie VAR var DANS une fonction, 
 // elle n'est utilisable que dans cette fonction
 
                             //========================================
-var TAILLE = Number;        // définition des variables GLOBALES    ==
-var T = Number;             //========================================
+//var TAILLE = 0;             // définition des variables GLOBALES    ==
+var T = 0;                  //========================================
+var n = 0;
+//var TAB = new Array;    défini dans la fonction initTab
+var MOT = "";
 
 
 
 GetInteger();
 
+
+
                             //  =======================================
                             //  ======      les fonctions       =======
                             //  =======================================   
 
-function GetInteger(){
-    TAILLE = window.prompt("Entrer une taille de tableau :");
+function GetInteger()
+    {
+    T = window.prompt("Entrer une taille de tableau :");
     InitTab;
     SaisieTab;
         console.log("GetInteger passée");
+    }
 
-}
 
-
-function InitTab(){
-    let T = parseInt(TAILLE);
+function InitTab()
+    {
     var TAB = new Array(T);
-    console.log("InitTab passée"+(TAILLE));
+        console.log("Tableau créé, taille =  ",T);
+    }
 
-}
 
-
-function SaisieTab(){
+function SaisieTab()
+    {
     for (let i = 0; i < (T); i++)
         console.log(i);
         TAB[i] = window.prompt(`Entrer valeur ${i}`);
-}
+    }
 
-/*
-function AfficheTab(){
+
+function AfficheTab()
+    {
     for (let i = 0; i < (T); i++)
         console.log(i);
         document.write("poste",i,"= :",TAB(i),("<br/>"));  
-}
+    }
 
 
 
-function RechercheTab(){
-    
-}
+//function RechercheTab(){} ============(affectée au bouton 2)
 
 
-function InfoTab(){
+function InfoTab()  //==== Calcul du maximum et de la moyenne des postes
+    {
 
-}
+    }
 
 
 
-*/
+
+
+
+//==========================================
+//====      la fonction RechercheTab    ====
+//==========================================
+
+function RechercheTab(n)
+{
+        TAB.splice(0,(n));                              //====          coupe les 'n' premiers caractères à partir du 'zero'
+
+        let MOT = TAB.shift();                          //====          shift renvoie le premier terme du tableau, et le supprime du même tableau
+                console.log("MOT = ",MOT);
+        return;
+
+
+}       
+//====      FIN  Fonction               ====
+
+
+
+//====      affichage du résultat   ====
+document.write('<br/>',"Le ",n," ième mot de la chaine ",TAB," est (",MOT,") voilà, c'est fini !");
+
+
+
+
+
+
+
+
 
                             //  =======================================
                             //  ======      les boutons       =========
                             //  =======================================   
 
-var bouton1= document.getElementById("Id_btn1");
-bouton1.addEventListener("click",clickbtn1);
 
-    function clickbtn1(){
+var btn1 = document.createElement("BUTTON");        // Créer un élément <button>
+var t = document.createTextNode("Tableau");         // Créer un noeud textuel
+btn1.appendChild(t);                                // Ajouter le texte au bouton
+document.body.appendChild(btn1);                    // Ajoute la balise <button> à la balise <body>
 
-}
-
-
-var bouton2= document.getElementById("Id_btn2");
-bouton2.addEventListener("click",clickbtn2);
-
-    function clickbtn2(){
-
-}
+        btn1.addEventListener("click",clickbtn1);
+        function clickbtn1()
+        {
+            console.log("bouton1");
+            AfficheTab()
+        }
 
 
 
-var bouton3= document.getElementById("Id_btn3");
-bouton3.addEventListener("click",clickbtn3);
+var btn2 = document.createElement("BUTTON");
+var t = document.createTextNode("Index");
+btn2.appendChild(t);
+document.body.appendChild(btn2);
 
-    function clickbtn3(){
+        btn2.addEventListener("click",clickbtn2);
+        function clickbtn2()
+        {
+            console.log("bouton2");
+            n = window.prompt("Entrer le num d'index : ");
+            RechercheTab(n);
+        }
 
-}
 
+var btn3 = document.createElement("BUTTON");
+var t = document.createTextNode("Moy Max");
+btn3.appendChild(t);
+document.body.appendChild(btn3);
+
+        btn3.addEventListener("click",clickbtn3);
+        function clickbtn3()
+        {
+            console.log("bouton3");
+
+        }
